1444c18319ef183936abfa710fcc9dc75427b5f3
Jhonas Moura <jhonaspradomoura@gmail.com>
2026-01-20 17:28:33 -0300

    fix: misc zsh

--- zsh/local.zsh
+++ zsh/local.zsh
@@ -13,3 +13,17 @@ eval "$(pyenv init - zsh)"
 
 eval "$(pyenv virtualenv-init -)"
 
+export PATH=$PATH:/opt/lua-language-server/bin
+export PATH=$HOME/.opencode/bin:$PATH
+
+# Ghostty shell integration for Bash. This should be at the top of your zsh!
+if [ -n "${GHOSTTY_RESOURCES_DIR}" ]; then
+    builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration"
+fi
+
+# Export opencode related environment variables
+export $(cat $HOME/dev/containers/opencode/.env | xargs)
+
+# Cabin completions
+fpath+=/home/jhonas/.local/share/cabin/completions/zsh
+autoload -Uz compinit && compinit 2>/dev/null || true
