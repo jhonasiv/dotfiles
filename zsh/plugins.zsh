83633075dd18bc5f05e68c698f6b316cbac86430
Jhonas Moura <jhonaspradomoura@gmail.com>
2026-02-22 15:13:41 -0300

    zsh: source plugin settings after plugin load

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,9 +6,11 @@ zpm load @gh/zpm-zsh/zpm-info
 
 # Auto suggestions
 zpm load @gh/zsh-users/zsh-autosuggestions,async
+source $ZDOTDIR/plugins/zsh-autosuggestions.zsh
 
 zpm load @gh/zdharma-continuum/fast-syntax-highlighting
 zpm load @gh/zsh-users/zsh-history-substring-search
+source $ZDOTDIR/plugins/zsh-history-substring-search.zsh
 
 zpm load @gh/olets/zsh-abbr
 
